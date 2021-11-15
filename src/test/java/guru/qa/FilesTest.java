98815db388125873eff36503bbd16a07af60bfcc
Dmitriy Kochurov <kraver90@gmail.com>
2021-11-16 00:45:26 +0500

    add fileTests

---
+++ src/test/java/guru/qa/FilesTest.java
@@ -0,0 +1,74 @@
+package guru.qa;
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import io.qameta.allure.*;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.*;
+
+import java.io.*;
+
+import static com.codeborne.pdftest.assertj.Assertions.assertThat;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static guru.qa.TestData.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class FilesTest extends TestBase {
+
+    @Test
+    @Owner("Кочуров Д.Е.")
+    @Feature("Работа с файлами")
+    @DisplayName("Имя файла отображается после загрузки")
+    void filenameShouldDisplayedAfterUploadActionFromClasspathTest() {
+        open(UPLOAD_PAGE);
+        $(".featured-btn").click();
+        $("input[id = 'file_0']").uploadFromClasspath(UPLOAD_FILE);
+        $(".uploadfile").shouldHave(text(UPLOAD_FILE));
+        $(".btn-success").click();
+        $(".upFile-info").shouldHave(text(UPLOAD_FILE));
+    }
+
+    @Test
+    @Owner("Кочуров Д.Е.")
+    @Feature("Работа с файлами")
+    @DisplayName("Скачивание текстового файла и проверка его содержимого")
+    void downloadSimpleFileTest() throws IOException {
+        open(DOWNLOAD_FILE_TXT);
+        File download = $("#downloadButton").download();
+        String fileContent = IOUtils.toString(new FileReader(download));
+        assertTrue(fileContent.contains(TXT_RESULT));
+    }
+
+    @Test
+    @Owner("Кочуров Д.Е.")
+    @Feature("Работа с файлами")
+    @DisplayName("Скачивание PDF файла")
+    void pdfFileDownloadTest() throws IOException {
+        open(DOWNLOAD_FILE_PDF);
+        File pdf = $(".left").download();
+        PDF parsedPdf = new PDF(pdf);
+        Assertions.assertEquals(190, parsedPdf.numberOfPages);
+//        Проверка по тексту
+//        assertThat(new PDF(pdf)).containsExactText(PDF_RESULT);
+    }
+
+    @Test
+    @Owner("Кочуров Д.Е.")
+    @Feature("Работа с файлами")
+    @DisplayName("Скачивание XLS файла")
+    void xlsFileDownloadTest() throws IOException {
+        open(DOWNLOAD_FILE_XLS);
+        File xls = $$("a[href*='pricelist/cement").find(text("Цемент")).download();
+        XLS parsedXls = new XLS(xls);
+        boolean checkPassed = parsedXls.excel
+                .getSheetAt(0)
+                .getRow(12)
+                .getCell(0)
+                .getStringCellValue()
+                .contains(XLS_RESULT);
+        assertTrue(checkPassed);
+    }
+}
---
+++ src/test/java/guru/qa/TestData.java
@@ -0,0 +1,13 @@
+package guru.qa;
+
+public class TestData {
+    public static String UPLOAD_PAGE = "https://www.file-upload.com/";
+    public static String DOWNLOAD_FILE_TXT = "https://www.mediafire.com/file/n72j4t7n4n2dsfe/testfile.txt/file";
+    public static String DOWNLOAD_FILE_PDF = "https://www.tutorialspoint.com/selenium/selenium_pdf_version.htm";
+    public static String DOWNLOAD_FILE_XLS = "https://stroimarket161.com/pricelist";
+    public static String UPLOAD_FILE = "example.txt";
+    public static String TXT_RESULT = "Получение списка рейтов отеля";
+    public static String PDF_RESULT = "Selenium is an open-source tool that is used for test automation";
+    public static String XLS_RESULT = "Цемент/Белый цемент";
+
+}
